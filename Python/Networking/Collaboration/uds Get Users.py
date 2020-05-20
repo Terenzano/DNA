acd8b129e088621d1cb357fb9648a3caf4608e92
Terence Parassi <naikatepa@gmail.com>
2020-05-20 16:54:54 +0300

    updated uds_Get_Users.py

--- Python/Networking/Collaboration/uds Get Users.py
+++ Python/Networking/Collaboration/uds Get Users.py
@@ -2,6 +2,8 @@ import requests
 import xml.dom.minidom
 import xmltodict
 import urllib3
+import json
+from pprint import pprint
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #### For use with the DevNet Sandbox CUCM 11.5 #####
 url = 'https://10.10.20.1/cucm-uds'
@@ -25,9 +27,19 @@ pretty = tree.toprettyxml()
 
 # Convert to Python Dict
 xmldata = xmltodict.parse(pretty)
-#print(json.dumps(xmldata, indent=2, sort_keys=True))
-users = xmldata['users']['user']
-for user in users:
-    print(f"{user['lastName']} {user['firstName']}")
-    print(f"ID: {user['id']}")
-    print(" ")
+print(json.dumps(xmldata, indent=2, sort_keys=True))
+print('*' * 50)
+
+# users = xmldata['users']['user']
+# for user in users:
+#     print(f"{user['lastName']} {user['firstName']}")
+#     print(f"ID: {user['id']}")
+#     print(" ")
+
+
+# Get specific userrrrrrrr
+
+user01 = xmldata['users']['user'][0]
+
+print("Here are User1 Details: ")
+print(json.dumps(user01, indent=2, sort_keys=True))
