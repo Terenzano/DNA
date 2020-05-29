c399528fe6cc0bcfb91be727e37aa91fde73d420
Terence Parassi <naikatepa@gmail.com>
2020-05-29 10:48:56 +0300

    Added fmc get networks.py

--- Python/Networking/Security/fmc get networks.py
+++ Python/Networking/Security/fmc get networks.py
@@ -18,7 +18,7 @@ token = resp_headers.get('X-auth-access-token', default=None)
 # Set the token in the headers to be used in the next call
 headers['X-auth-access-token'] = token
 
-apps_url = '/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/object/applications'
+apps_url = '/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/object/networks'
 apps_response = requests.get(
     f'{url}{apps_url}', headers=headers, verify=False).json()
 print(json.dumps(apps_response, indent=2, sort_keys=True))
